cb46c8e2404d9d3b108839193c9b05d0005e831d
Ismael Fernandez Fernandez <a23ismaelff@teis.intranet>
2025-10-03 19:24:46 +0200

    inicio v2

--- src/PasaLaCalculadora.java
+++ src/PasaLaCalculadora.java
@@ -2,36 +2,102 @@ import java.util.Scanner;
 
 public class PasaLaCalculadora {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Introduce el numero maximo este debe ser superior a 10 e inferior a 99:");
-        int numMaximo=sc.nextInt();
-        int numTotal=0;
-        while(numMaximo>=numTotal){
-            System.out.println("Introduce un numero: ");
-            int jugador1= sc.nextInt();
-            if (jugador1<  0 & jugador1 !=numMaximo){
-                jugador1 = (int) Math.round(Math.random()*89)+10;
+        //objetivo
+        int numMaximo;
+        int numTotal = 0;
+        int jugador1;
+        int jugador2;
+        Scanner sc = new Scanner(System.in);
+
+        numMaximo = numeroObjetivo(sc);
+        //partida
+
+
+        //primer turno
+        while (true){
+            System.out.println("\u001B[34m"+"Introduce el primer numero jugador 1: ");
+            jugador1 = sc.nextInt();
+
+            if (jugador1>0&jugador1<10){
                 numTotal+=jugador1;
+                System.out.println(numTotal);
+                restricciones();
+                break;
             }
-            else if (jugador1> 0 & jugador1 !=numMaximo){
+        }
+
+        //resto juego
+
+
+        while (numMaximo >= numTotal) {
+
+            //turno jugador 2
+
+            System.out.println("\u001B[35m"+"Introduce un numero jugador 2: ");
+             jugador2 = sc.nextInt();
+
+            if (jugador2>0&jugador2<10){
+                numTotal+=jugador2;
+                System.out.println(numTotal);
+
+                if (numTotal>=numMaximo){
+                    System.out.println("Perdedor jugador 2 ;)");
+                    break;
+                }
+
+            } else {
+                System.out.println("numero incorrecto, pierdes turno");
+                System.out.println(numTotal);
+
+            }
+
+            // turno jugador 1
+
+            System.out.println("\u001B[34m"+"Introduce un numero jugador 1: ");
+            jugador1 = sc.nextInt();
+
+            if (jugador1>0&jugador1<10){
                 numTotal+=jugador1;
-            }
-            else {
+                System.out.println(numTotal);
+                if (numTotal>=numMaximo){
+                    System.out.println("Perdedor jugador 1 ;)");
+                    break;
+                }
+
+            } else {
+                System.out.println("numero incorrecto, pierdes turno");
+                System.out.println(numTotal);
 
             }
 
+        }
+    }
 
-            System.out.println("Introduce un numero: ");
-            int jugador2= sc.nextInt();
-            if (jugador2>  0){
-                jugador2 = (int) Math.round(Math.random()*99);
-                numTotal+=jugador2;
+    private static int numeroObjetivo(Scanner sc) {
+        int numMaximo;
+        while (true) {
+
+            System.out.println("\u001B[32m"+"Introduce el numero maximo este debe ser superior a 10 e inferior a 99:");
+            numMaximo = sc.nextInt();
+
+            if (numMaximo == -1) {
+                numMaximo = (int) Math.round(Math.random() * 89) + 10;
+                break;
+            } else if (numMaximo>10 & numMaximo <99) {
+                numMaximo=numMaximo;
+                break;
+            }else {
+                System.out.println("\u001B[31m"+"Que sea correcto ;}");
             }
-            else {
-                numTotal+=jugador2;
-            }
 
+        }
+        return numMaximo;
+    }
 
+    //restricciones
+    private static void restricciones() {
+        if (jugador2==jugador1){
+            System.out.println();
         }
     }
 }
