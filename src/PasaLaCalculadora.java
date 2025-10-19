23a73e87cce20e08f8642a6050075aef1b9da98b
Ismael <ismaelfdez27@gmail.com>
2025-10-19 21:15:06 +0200

    Eliminacion de funciones no utilizadas

--- src/PasaLaCalculadora.java
+++ src/PasaLaCalculadora.java
@@ -3,67 +3,86 @@ import java.util.Scanner;
 
 public class PasaLaCalculadora {
     public static void main(String[] args) {
-        //objetivo
-        int numMaximo;
+
+        int numMax;
         int numTotal = 0;
         int numeroInsertado = 0;
         int numUltimo = 0;
-        int numeroJugadores ;
-        String jugador1="";
-        String jugador2="";
-        String jugador3="";
+        int numeroJugadores;
+        String jugador1 = "";
+        String jugador2 = "";
+        String jugador3 = "";
+
+
 
         Scanner sc = new Scanner(System.in);
-        //
-        System.out.println("ingrese el numero de jugadores:");
-        numeroJugadores= sc.nextInt();
-        if(numeroJugadores==2){
+        //peticion al usuario del numero de jugadores
+        System.out.println("ingrese el numero de jugadores, en caso de ser más de tres no podras poner nombres a los jugadores:");
+        numeroJugadores = sc.nextInt();
+
+        //asignacion de nombres
+        if (numeroJugadores == 2) {
             System.out.println("ingrese su nombre jugador 1");
-            jugador1=sc.next();
+            jugador1 = sc.next();
             System.out.println("ingrese su nombre jugador 2");
-            jugador2=sc.next();
+            jugador2 = sc.next();
         }
-        if (numeroJugadores==3){
+        if (numeroJugadores == 3) {
             System.out.println("ingrese su nombre jugador 1");
-            jugador1=sc.next();
+            jugador1 = sc.next();
             System.out.println("ingrese su nombre jugador 2");
-            jugador2=sc.next();
+            jugador2 = sc.next();
             System.out.println("ingrese su nombre jugador 3");
-            jugador3=sc.next();
+            jugador3 = sc.next();
         }
-        String[]jugadores={jugador1, jugador2, jugador3};
+        String[] jugadores = {jugador1, jugador2, jugador3};
+
         while (true) {
-            numMaximo = numeroObjetivo(sc);
-           // numTotal = primerTurno(numeroInsertado, numTotal, sc, jugador1);
-           // numUltimo = numTotal;
+            numMax = obtenerNumeroObjetivo(sc);
 
             while (true) {
 
                 for (int i = 0; i < numeroJugadores; i++) {
-                    System.out.println("\u001B[35m" + "Introduce un numero " + jugadores[i] +": ");
+                    //2 y 3 jugadores van a poder poner su nombre mientras que mas de estos numeros no
+                    if (numeroJugadores == 2 | numeroJugadores == 3) {
+                        System.out.println("\u001B[35m" + "Introduce un numero " + jugadores[ i ] + " : ");
+                    } else {
+                        System.out.println("\u001B[35m" + "Introduce un numero jugador" + i + " : ");
+                    }
                     numeroInsertado = sc.nextInt();
-                    while (validacion(numeroInsertado,numUltimo) == 1) {
-                        System.out.println("numero incorrecto, vuelva a insertar un numero "+ jugadores[i]);
+
+                    while (validarNumero(numeroInsertado, numUltimo) == 1) {
+
+                        if (numeroJugadores == 2 | numeroJugadores == 3) {
+                            System.out.println("\u001B[35m" + "numero incorrecto, vuelva a insertar un numero " + jugadores[i] + " : ");
+                        } else {
+                            System.out.println("\u001B[35m" + "numero incorrecto, vuelva a insertar un numero jugador" + i + " : ");
+                        }
+
+
                         numeroInsertado = sc.nextInt();
                     }
 
-                        numTotal += numeroInsertado;
-                        numUltimo = numeroInsertado;
+                    numTotal += numeroInsertado;
+                    numUltimo = numeroInsertado;
                     System.out.println(numTotal);
 
-                    if (numTotal >= numMaximo) {
-                        System.out.println("Perdedor jugador " +  jugadores[i]);
+                    if (numTotal >= numMax) {
+                        if (numeroJugadores == 2 | numeroJugadores == 3) {
+                            System.out.println("Perdedor  " + jugadores[i]);
+                        }else{
+                            System.out.println("Perdedor jugador" + i);
+                        }
                         break;
                     }
                 }
-                if (numTotal >= numMaximo) {
+                if (numTotal >= numMax) {
                     break;
                 }
             }
 
-
-            //jugar otra partida
-            System.out.println("Si desea continuar escriba `Y´ si no pulse cualquier tecla.");
+            //peticion al usuario de la letra Y para poder jugar otra partida
+            System.out.println("Si desea continuar escriba `Y´, en caso contrario pulse otra tecla.");
             Scanner cr = new Scanner(System.in);
             String continuar = cr.nextLine();
 
@@ -78,44 +97,25 @@ public class PasaLaCalculadora {
 
     }
 
-    /**
-     * El primer turno no valida como el resto por eso está separado.
-     *
-     * @param numeroInsertado
-     * @param numUltimo
-     * @param sc
-     * @return
-     */
-    public static int primerTurno(int numeroInsertado, int numUltimo, Scanner sc, String jugador1) {
-
-        System.out.println("\u001B[34m" + "Introduce el primer numero "+ jugador1 + " :");
-        numeroInsertado = sc.nextInt();
-
-        if (numeroInsertado > 0 & numeroInsertado < 10) {
-            numUltimo = numeroInsertado;
-        }
-        return numUltimo;
-    }
 
     /**
      * pedimos al usuario el numero objetivo
      *
      * @param sc
-     * @return numMaximo
+     * @return numMaximo numero objetivo a no superar
      */
-    //peticion al usuario del numero objetivo
-    private static int numeroObjetivo(Scanner sc) {
+    private static int obtenerNumeroObjetivo(Scanner sc) {
         int numMaximo;
         while (true) {
 
             System.out.println("\u001B[32m" + "Introduce el numero maximo este debe ser superior a 10 e inferior a 99:");
             numMaximo = sc.nextInt();
-
+            //numero random
             if (numMaximo == -1) {
                 numMaximo = (int) Math.round(Math.random() * 89) + 10;
                 break;
+                //comprobacion para el numero objetivo
             } else if (numMaximo > 10 & numMaximo < 99) {
-                numMaximo = numMaximo;
                 break;
             } else {
                 System.out.println("\u001B[31m" + "Que sea correcto ;}");
@@ -133,12 +133,11 @@ public class PasaLaCalculadora {
      * El numero insertado tiene que estar en la misma fila o columna que el numero anterior
      *
      * @param numeroInsertado
-
      * @param numUltimo
      * @return 0 si numeroInsertado es valido
      * @return 1 si numeroInsertado no es valido
      */
-    public static int validacion(int numeroInsertado, int numUltimo) {
+    public static int validarNumero(int numeroInsertado, int numUltimo) {
 
         if (numeroInsertado != numUltimo) {
             switch (numUltimo) {
@@ -207,6 +206,7 @@ public class PasaLaCalculadora {
                     }
                     break;
                 }
+                //solo se utiliza en el primer turno
                 case 0: {
                     if (numeroInsertado > 0 & numeroInsertado < 10) {
                         System.out.println("El numero insertado cumple las normas");
@@ -214,13 +214,9 @@ public class PasaLaCalculadora {
                     }
                     break;
                 }
-
             }
         }
-
         return 1;
-
-
     }
 }
 
